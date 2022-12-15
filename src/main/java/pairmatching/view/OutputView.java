c40130f31a24ebe67a9f9a96d67ed6d2232cb009
wnsgml7267 <dlwns7267@naver.com>
2022-12-15 19:41:07 +0900

    feat(view/OutputView): 출력 값 받는 기능 구현

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,5 +1,27 @@
 package pairmatching.view;
 
 public class OutputView {
+    public static final String RESULT_PAIRS = "페어 매칭 결과입니다.";
+    public static final String RESET = "초기화 되었습니다.";
+
+    public static void printPairs(Pairs pairs) {
+        System.out.println(RESULT_PAIRS);
+        System.out.println(pairs);
+        printBreak();
+    }
+
+    public static void printReset() {
+        System.out.println(RESET);
+        printBreak();
+    }
+
+    public static void printError(String message) {
+        System.out.println("[ERROR] " + message);
+        printBreak();
+    }
+
+    public static void printBreak() {
+        System.out.println();
+    }
 
 }
