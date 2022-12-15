3f30a01ecd9ed8d4f169f3b1ba1056059053fd9d
wnsgml7267 <dlwns7267@naver.com>
2022-12-15 21:11:19 +0900

    feat(utils/Converter): Converter 클래스 생성

---
+++ src/main/java/pairmatching/utils/Converter.java
@@ -0,0 +1,11 @@
+package pairmatching.utils;
+
+public class Converter {
+    public static int getInt(String numberString) {
+        return Integer.parseInt(numberString);
+    }
+
+    public static String getString(int number) {
+        return Integer.toString(number);
+    }
+}
