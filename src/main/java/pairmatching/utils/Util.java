9e4f655b39f9de21456ad5a57252dfcd8129e3d5
wnsgml7267 <dlwns7267@naver.com>
2022-12-15 21:05:19 +0900

    feat(utils/Util): 유틸 랜덤 기능 구현

--- src/main/java/pairmatching/utils/Util.java
+++ src/main/java/pairmatching/utils/Util.java
@@ -1,5 +1,11 @@
 package pairmatching.utils;
 
-public class Util {
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+
+public class Util {
+    public static List<String> shuffle(List<String> list) {
+        return Randoms.shuffle(list);
+    }
 }
